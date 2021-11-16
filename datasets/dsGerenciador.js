4fed1af757c7afcec19c988cea48954d01645541
Guilherme-dsr <guilhermesilva.dsr@gmail.com>
2021-11-15 21:57:12 -0300

    first commit

---
+++ datasets/dsGerenciador.js
@@ -0,0 +1,18 @@
+function defineStructure() {
+
+}
+function onSync(lastSyncDate) {
+
+}
+function createDataset(fields, constraints, sortFields) {
+	
+	var ds = DatasetBuilder.newDataset();
+	ds.addColumn("servico");
+	
+	ds.addRow(new array("fluig"));
+	
+	return ds;
+
+}function onMobileSync(user) {
+
+}
